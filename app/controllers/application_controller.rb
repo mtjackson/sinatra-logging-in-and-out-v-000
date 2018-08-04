89c6bc07a20fb53825e1881140403a87041cef3d
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-04 15:43:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"])
-    if @user && @user.password == params["password"]
+    if @user && @user.password == params[:password]
       session[:user_id] = @user.user_id
       redirect '/account'
     else
