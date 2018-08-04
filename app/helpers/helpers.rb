5324e5d28941df6b1c164a88a4ca1fb79cc317ec
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-04 15:34:44 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
   def self.current_user(session)
-    current_user = User.find_by(user_id: session["user_id"])
+    current_user = User.find_by_id(user_id session["user_id"])
     current_user
   end
 
